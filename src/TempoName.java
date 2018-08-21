6d9c14cf1e682b70cf98fd97e3549798f24391d5
Maria <maria.vehero@gmail.com>
2018-08-21 14:48:06 +0300

    Commit1

---
+++ src/TempoName.java
@@ -0,0 +1,9 @@
+public class TempoName {
+    public static void main (String[] args) {
+        int testVar;
+        testVar = 3;
+        int testTwo;
+        testTwo = (2 + testVar) * 3;
+        System.out.println(testTwo);
+        }
+}
